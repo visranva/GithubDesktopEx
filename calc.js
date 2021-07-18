ee0402f0a2874055974c4e7edc3aa5ad1f549488
Viswa <visran@gmail.com>
2021-07-18 19:50:55 +0530

    Create calc.js
    This is a calculator js file

---
+++ calc.js
@@ -0,0 +1,16 @@
+
+function add(par1, par2){
+    return par1+par2
+}
+
+function sub(par1,par2){
+    return par1-par2
+}
+
+function multiply(){
+    return par1*par2
+})
+
+function divide(){
+    return par1/par2
+}
